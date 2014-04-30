1855c6e8f3be1659725de6e64a1cc72122d9aa8e
YuanQY <yuanqingyun@gmail.com>
2014-04-30 15:03:52 +0800

    mkvendor.sh: Initial commit of hongmi

---
+++ cm.mk
@@ -0,0 +1,18 @@
+## Specify phone tech before including full_phone
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := hongmi
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/xiaomi/hongmi/device_hongmi.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := hongmi
+PRODUCT_NAME := cm_hongmi
+PRODUCT_BRAND := xiaomi
+PRODUCT_MODEL := hongmi
+PRODUCT_MANUFACTURER := xiaomi
